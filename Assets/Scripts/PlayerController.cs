a7b43b04b344c26f657145b78de6e6e8d9a69b12
Lufer <zuyeu20319@student.pswbp.pl>
2021-06-01 19:57:48 +0300

    op

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private float _speed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ignored
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var _moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.position += _moveInput * Time.deltaTime * _speed;
+    }
+
+}
