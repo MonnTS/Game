44cb11b7869e9afe3652bac61db7549d47007dc7
Monn <zujev.daniel20319@gmail.com>
2021-06-29 01:36:20 +0300

    Updated class PlayerController.cs and FirstLevel.unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,25 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 5f; // Initialize current speed for the player
-    public Rigidbody2D rb; // Getting rigidbody for the object player
-    Vector2 movement; // 2D Vector
+    // Initialize current speed for the player
+    private const float MovementSpeed = 5.0f;
+    // Getting rigidbody for the object player
+    public Rigidbody2D rb;
+    // 2D Vector
+    private Vector2 _movement;
     
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        /* Getting x and y from the object */ 
-        movement.x = Input.GetAxisRaw("Horizontal"); 
-        movement.y = Input.GetAxisRaw("Vertical");
+        // Getting X, Y from the object
+        _movement.x = Input.GetAxisRaw("Horizontal"); 
+        _movement.y = Input.GetAxisRaw("Vertical");
     }
 
     // FixedUpdate called for physics calculation
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        // Formula for movement of the object Player  
-        rb.MovePosition(rb.position + movement * (movementSpeed * Time.fixedDeltaTime));
+        // Formula for movement of the object Player
+        rb.MovePosition(rb.position + _movement * (MovementSpeed * Time.fixedDeltaTime));
     }
 }
