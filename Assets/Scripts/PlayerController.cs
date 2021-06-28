c0953c5b75f8571ee2372bfe764004f8c9c30c27
Monn <zujev.daniel20319@gmail.com>
2021-06-29 01:27:05 +0300

    ~

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [FormerlySerializedAs("_speed")] public float movementSpeed = 5f;
-    [FormerlySerializedAs("_rb")] public Rigidbody2D rb;
-    Vector2 movement;
+    public float movementSpeed = 5f; // Initialize current speed for the player
+    public Rigidbody2D rb; // Getting rigidbody for the object player
+    Vector2 movement; // 2D Vector
     
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
+        /* Getting x and y from the object */ 
+        movement.x = Input.GetAxisRaw("Horizontal"); 
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
+    // FixedUpdate called for physics calculation
     void FixedUpdate()
     {
+        // Formula for movement of the object Player  
         rb.MovePosition(rb.position + movement * (movementSpeed * Time.fixedDeltaTime));
     }
 }
