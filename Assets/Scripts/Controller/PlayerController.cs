2d275dc8e4c51d334f74a9e24e11ad5124c1defc
Monn <zujev.daniel20319@gmail.com>
2021-06-29 01:59:58 +0300

    ~

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Initialize current speed for the player
+        private const float MovementSpeed = 5.0f;
+        // Getting rigidbody for the object player
+        public Rigidbody2D rb;
+        // 2D Vector
+        private Vector2 _movement;
+    
+        // Update is called once per frame
+        private void Update()
+        {
+            // Getting X, Y from the object
+            _movement.x = Input.GetAxisRaw("Horizontal"); 
+            _movement.y = Input.GetAxisRaw("Vertical");
+        }
+
+        // FixedUpdate called for physics calculation
+        private void FixedUpdate()
+        {
+            // Formula for movement of the object Player
+            rb.MovePosition(rb.position + _movement * (MovementSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
