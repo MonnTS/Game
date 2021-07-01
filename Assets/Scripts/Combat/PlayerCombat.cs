ab1679415d1d4bfe6a7424eb23997905bba88ae8
Monn <zujev.daniel20319@gmail.com>
2021-07-01 23:34:50 +0300

    Refactored class PlayerController.cs and created new class PlayerCombat.cs + Updated a name for the player.

---
+++ Assets/Scripts/Combat/PlayerCombat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Combat
+{
+    public class PlayerCombat : MonoBehaviour
+    {
+        public Animator animator;
+        // Getting a trigger with the name "Attack" in efficient way.
+        private static readonly int Attack = Animator.StringToHash("Attack");
+        
+        public void Update()
+        {
+            if (!Input.GetMouseButtonDown(0)) return;
+            Combat();
+            Debug.Log("You pressed left button.");
+        }
+
+        private void Combat()
+        {
+            animator.SetTrigger(Attack);
+        }
+    }
+}
--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -4,29 +4,17 @@ namespace Controller
 {
     public class PlayerController : MonoBehaviour
     {
-        // Initialize current speed for the player
-        private const float MovementSpeed = 5.0f;
-        // Getting rigidbody of the object player
+        private float _movementSpeed = 5.0f;
         public Rigidbody2D rigidBody;
         public Animator animator;
-        // 2D Vector
         private Vector2 _movement;
-        // String based property lookup is inefficient in animator, so we will refactor the code just a little.
-        // Converting string to int, which is our variables in animator. 
+        // Getting a trigger with the name "Attack" in efficient way
         private static readonly int Horizontal = Animator.StringToHash("Horizontal");
         private static readonly int Vertical = Animator.StringToHash("Vertical");
         private static readonly int Speed = Animator.StringToHash("Speed");
-        private static readonly int Attack1 = Animator.StringToHash("Attack");
-
-        // Update is called once per frame
+        
         public void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Attack();
-                Debug.Log("Left button pressed");
-            }
-            
             // Getting X, Y from the object
             _movement.x = Input.GetAxisRaw("Horizontal"); 
             _movement.y = Input.GetAxisRaw("Vertical");
@@ -37,17 +25,10 @@ namespace Controller
             animator.SetFloat(Speed, _movement.sqrMagnitude);
         }
 
-        // FixedUpdate called for physics calculation
         public void FixedUpdate()
         {
             // Formula for movement of the object Player
-            rigidBody.MovePosition(rigidBody.position + _movement * (MovementSpeed * Time.fixedDeltaTime));
-        }
-        
-        // Method for the attack
-        private void Attack()
-        {
-            animator.SetTrigger(Attack1);
+            rigidBody.MovePosition(rigidBody.position + _movement * (_movementSpeed * Time.fixedDeltaTime));
         }
     }
 }
