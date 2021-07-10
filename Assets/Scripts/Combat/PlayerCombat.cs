c627d0a31eb2df8de4d9f9bde4ca82b910a82db9
Monn <zujev.daniel20319@gmail.com>
2021-07-10 17:35:46 +0300

    New class EnemyData.cs + updated PlayerCombat.cs

--- Assets/Scripts/Combat/PlayerCombat.cs
+++ Assets/Scripts/Combat/PlayerCombat.cs
@@ -1,22 +1,46 @@
-﻿using UnityEngine;
+﻿using Data;
+using UnityEngine;
 
 namespace Combat
 {
     public class PlayerCombat : MonoBehaviour
     {
         public Animator animator;
+        public Transform attackPoint;
+        public LayerMask enemyLayer;
+        
+        private const float RangeAttack = 0.5f;
+        private const float AttackRate = 2f;
+        private float _nextAttackTime = 0f;
+        private int _attackDamage = 15;
+        
         // Getting a trigger with the name "Attack" in an efficient way.
         private static readonly int Attack = Animator.StringToHash("Attack");
         
         public void Update()
         {
+            if (!(Time.time >= _nextAttackTime)) return;
             if (!Input.GetMouseButtonDown(0)) return;
             Combat();
+            _nextAttackTime = Time.time + 1f / AttackRate;
         }
 
         private void Combat()
         {
             animator.SetTrigger(Attack);
+            var hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, RangeAttack,
+                enemyLayer);
+
+            foreach (var enemy in hitEnemies)
+            {
+                enemy.GetComponent<EnemyData>().Damage(_attackDamage);
+            }
+        }
+
+        public void OnDrawGizmosSelected()
+        {
+            if (attackPoint == null) return;
+            Gizmos.DrawWireSphere(attackPoint.position, RangeAttack);
         }
     }
 }
---
+++ Assets/Scripts/Data/EnemyData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    public class EnemyData : MonoBehaviour
+    {
+        private int _currentHealth;
+        private int _enemyHealth = 100;
+        private SpriteRenderer _spriteRenderer;
+        void Start()
+        {
+            _currentHealth = _enemyHealth;
+        }
+
+        public void Damage(int damage)
+        {
+            _currentHealth -= damage;
+            if(_currentHealth <= 0)
+                Death();
+        }
+
+        private void Death()
+        {
+            var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach(var enemy in enemies)
+            {
+                Destroy(enemy);
+            }
+        }
+    }
+}
+
