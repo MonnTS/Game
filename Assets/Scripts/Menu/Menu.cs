1d868a2970cba68b8d857f2d983321a9d955ccd1
Monn <zuyeu20319@student.pswbp.pl>
2021-07-19 20:28:17 +0300

    ~

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class Menu : MonoBehaviour
+    {
+        public void btn_Play()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void btn_Exit()
+        {
+            Debug.Log("Yes");
+            Application.Quit();
+        }
+    }   
+}
+ 
