4999ef84f6b3599117f7e52ef1facd72c6b4bcf7
Daniel <73000732+MonnTS@users.noreply.github.com>
2021-07-22 01:05:13 +0300

    New Interface ISettings.cs

---
+++ Assets/Scripts/Interfaces/ISettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Interfaces
+{
+    public interface ISettings
+    {
+        void btn_Play();
+        void btn_Exit();
+        void ck_FullScreen();
+        void ck_Windowed();
+        void ck_MuteMusic();
+        void ck_MuteSounds();
+    }
+}
