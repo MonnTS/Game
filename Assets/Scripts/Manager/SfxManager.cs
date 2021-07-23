d91d8f2ddbd4f695eedb0ae8062ae551f56d7815
Daniel <73000732+MonnTS@users.noreply.github.com>
2021-07-23 21:26:32 +0300

    New Manager class for the SFX.

---
+++ Assets/Scripts/Manager/SfxManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Manager
+{
+    public class SfxManager : MonoBehaviour
+    {
+        #region FIELDS
+
+        public AudioSource audioSource;
+
+        [SerializeField] private float musicVolume = 1f;
+
+        #endregion
+
+        #region UNITYMETHODS
+
+        private void Awake()
+        {
+            audioSource.Play();
+        }
+
+        private void Update()
+        {
+            audioSource.volume = musicVolume;
+        }
+
+        #endregion
+        
+        #region METHODS
+
+        public void UpdateVolume(float volume)
+        {
+            musicVolume = volume;
+        }
+
+        #endregion
+    }
+}
