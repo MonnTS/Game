a0f5b1a148eb5b1a7b6b86e6501b86dd0684742f
Daniel <73000732+MonnTS@users.noreply.github.com>
2021-07-23 21:27:04 +0300

    New class for the pause menu.

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Menu
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public void btn_Back(){}
+        public void btn_Settings(){}
+        public void btn_ExitToMainMenu(){}
+    }
+}
