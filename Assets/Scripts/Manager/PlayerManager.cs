ac0479cdccce1542df19a516ece77b27275f4cde
Daniel <73000732+MonnTS@users.noreply.github.com>
2021-07-29 20:58:13 +0300

    New class PlayerManager.cs

---
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Manager
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        #region FIELDS
+        
+        [SerializeField] private GameObject deathScreen;
+        [SerializeField] private AudioSource audioSource;
+        [SerializeField] private GameObject menuCanvas;
+
+        #endregion
+
+        #region METHODS
+
+        public void Death()
+        {
+            deathScreen.SetActive(true);
+            menuCanvas.SetActive(false);
+            gameObject.SetActive(false);
+            audioSource.volume = 0f;
+            Time.timeScale = 0f;
+        }
+
+        #endregion
+        
+    }
+}
