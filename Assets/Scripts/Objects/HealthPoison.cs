79c01ed66aa0342280ec7050811d9397542acae8
Daniel <zuyeu20319@student.pswbp.pl>
2021-08-02 21:54:48 +0300

    New class for the health poison.

---
+++ Assets/Scripts/Objects/HealthPoison.cs
@@ -0,0 +1,32 @@
+using Data;
+using UnityEngine;
+
+namespace Objects
+{
+    public class HealthPoison : MonoBehaviour
+    {
+        #region FIELDS
+
+        private static int _currentValue;
+        private int _maxValue;
+        
+        #endregion
+
+        #region UNITYMETHODS
+
+        private void Update()
+        {
+            _currentValue = PlayerData.CurrentHealth;
+            _maxValue = PlayerData.MAXHealth;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if(!other.collider.CompareTag("Player")) return;
+            if (_currentValue == _maxValue) return;
+            Destroy(gameObject);
+        }
+
+        #endregion
+    }
+}
